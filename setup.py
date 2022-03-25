0dda53e0fd2355ebe7018b7ee8f34380f9f655d5
rami albasha <rami.albasha@inrae.fr>
2022-03-25 15:59:15 +0100

    installation cleanup + pep

--- setup.py
+++ setup.py
@@ -1,47 +1,31 @@
-import sys, os
-from setuptools import setup, find_packages
-from os.path import join as pj
-
 from openalea.deploy.metainfo import read_metainfo
+from setuptools import setup, find_packages
 
 # Reads the metainfo file
 metadata = read_metainfo('metainfo.ini', verbose=True)
-for key, value in metadata.iteritems():
-    exec("%s = '%s'" % (key, value))
 
-setup(name=name,
-      version=version,
-      description=description,
-      long_description=long_description,
-      authors=authors,
-      authors_email=authors_email,
-      license=license,
-      namespace_packages=[namespace],
-      create_namespaces=True,
-      # packages = [pkg_name],
-      zip_safe=False,
-      packages=['alinea.topvine',
-                'alinea.topvine.carto',
-                'alinea.topvine.demo',
-                'alinea.topvine.macro',
-                'alinea.topvine.farquhar',
-                'alinea.topvine.farquhar.demo',
-                'alinea.topvine.lsystem',
-                'alinea.topvine.geom',
-                'alinea.topvine.farquhar',
-                'alinea.topvine.farquhar.demo',
-                'alinea.topvine.farquhar.macro',
-                'alinea.topvine.law',
-                'alinea.topvine.rammoy',
-                'alinea.topvine.digit',
-                'alinea.topvine.data'],
+setup(
+    name=metadata['name'],
+    version=metadata['version'],
+    description=metadata['description'],
+    long_description=metadata['long_description'],
+    author=metadata['authors'],
+    author_email=metadata['authors_email'],
+    url=metadata['url'],
+    license=metadata['license'],
+    keywords=metadata['keywords'],
 
-      package_dir={'alinea.topvine': 'topvine', },
-      package_data={'': ['*.csv', '*.8', '*.d3d', '*.png', '*.lsys']},
-      entry_points={'wralea': ['topvine = alinea.topvine', ]},
+    # package installation
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
 
-      # Dependencies
-      setup_requires=['openalea.deploy'],
-      install_requires=[],
-      dependency_links=['http://openalea.gforge.inria.fr/pi'],
-      )
+    # Namespace packages creation by deploy
+    namespace_packages=[metadata['namespace']],
+    create_namespaces=True,
+    zip_safe=False,
+
+    # Dependencies
+    setup_requires=['openalea.deploy'],
+    dependency_links=['http://openalea.gforge.inria.fr/pi'],
+    install_requires=[]
+)
