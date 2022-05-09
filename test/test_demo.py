c7e27220be4e2bbd2cdd272014e5341e6f6d2218
jazzsta <44723405+jazzsta@users.noreply.github.com>
2022-05-09 15:09:55 +0200

    changements

--- test/test_demo.py
+++ test/test_demo.py
@@ -1,6 +1,9 @@
 """test generated from topvine demo wwralea"""
 import alinea.topvine.data_samples as ds
+from alinea.topvine.stand_generator import stand_generator
 from alinea.topvine.vine_topiary import vine_topiary
+from alinea.topvine.write_geom_file import write_geom_file
+
 
 def test_demo_topvine_static():
     vt = vine_topiary()
@@ -8,3 +11,8 @@ def test_demo_topvine_static():
     allometry = ds.allometry_file()
     dl = ds.dl_file()
     scene = vt(tab_shoot, dl, allometry, False, False, False)
+
+def test_demo_stand_generator():
+    carto = read_stand_file(r'./src/alinea/topvine/carto/carto.csv')
+    tabgeom = stand_generator(, CxT, personalise, nbr, nbp)
+    write_geom_file(tabgeom,"test_generated_stand.csv")
