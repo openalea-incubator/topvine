a3f31a9ccf2e518378827535ecd30a04e969bdcf
jazzsta <44723405+jazzsta@users.noreply.github.com>
2022-07-19 10:09:15 +0200

    generateur de scene 2022

---
+++ test/top_vine_scene_2022.py
@@ -0,0 +1,70 @@
+from alinea.topvine.gen_normal_canopy import gen_normal_canopy
+from alinea.topvine.gen_shoot_param import gen_shoot_param
+from alinea.topvine.translate_shoots import translate_shoots
+from alinea.topvine.vine_topiary import vine_topiary
+import alinea.topvine.data_samples as ds
+
+
+def top_vine_scene_2022(stand_address='/data/carto.csv', dl_shoot_address='/data/2W_VSP_GRE_ramd.csv',
+                        dl_address='/data/Law-leaf-2W-Grenache.csv', shoot_address='/data/ex_rammoy3.csv',
+                        allom_address='/data/allo_Grenache.csv', branches=True, trunk=True):
+    # a function that generates all types of scenes based on diverse input
+
+    """
+    1)	Plot file  (stand file)    --->   carto
+
+        :   For every plant, XYZ coordinates + number of branches
+
+    2)	Mean shoot file (shoot file)  --->  ram_moy
+
+        :   1st line : number of phytomers
+            Then     : number of secondary leaves, relative position, leaf surface area
+
+    3)	Distribution laws for shoot parameters (dl shoot file) ---> 2W_VSP_GRE_ramd
+
+        :   X0,Y0,Z0    : distribution laws for the positioning of the spurs.
+            DX,DY,DZ    : distribution laws for the distancing of the buds in the spurs.
+            Dist        : seems not to be used
+            freq AZI    : frequency of shoot AZI of angle (-20, 20), (20, 160), (160, 200) and (200, 340)
+            x (     )   : means of the 4 other shoot parameters, namely initial elevation, angle between basal and distal tangents (a.k.a curvature), proportion of shoot accounting for half the curvature and normalized length.
+            S (    )    : Covariance matrices for the 4 other shoot parameters for each azimuth range.
+
+    4)	Distribution laws for leaf parameters (dl file)  --->  Law-leaf-2W-Grenache
+
+        :   Elevation South – Elevation North – Azimuth South – Azimuth North
+
+    5)	Shoot parameter file (geom)  ---> ex_geom2
+
+        :   For every shoot, XYZ coordinates + values of 5 parameters (Azi, Elv, Crv, Prp, Lng)
+
+    6)	Allometry file ---> allo_Grenache
+
+        :   The first line includes the allometric parameters a & b that link the length of a shoot with its number of phytomers (L = a ⋅n+b).
+
+    """
+
+    def _stand_generator(carto, spurs0, dspurs, f_azi, shoot):
+        geom = []
+        generator = gen_shoot_param()
+        translator = translate_shoots()
+        for v, n in carto:
+            shoot_params = generator(n, spurs0, dspurs, f_azi, shoot)
+            translated = translator(shoot_params, v)
+            geom.append(translated)
+        return geom
+
+    # demo
+    carto = ds.stand_file(stand_address)  # [posxyz_plant, nb_coursons]
+    spurs0, dspurs, f_azi, shoot = ds.dl_shoot_file(dl_shoot_address)
+    geom = _stand_generator(carto, spurs0, dspurs, f_azi, shoot)
+    # write_geom(geom)
+
+    vt = vine_topiary()
+    generator = gen_normal_canopy()
+    dl = ds.dl_file(dl_address)
+    allometry = ds.allometry_file(allom_address)
+    shoot = ds.shoot_file(shoot_address)
+    tab_shoot = generator(geom, shoot, dl)
+    scene = vt(tab_shoot, dl, allometry, branches, trunk, False)   # the last parameter, "False", does nothing
+
+    return geom
