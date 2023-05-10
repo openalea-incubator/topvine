87e9b0669f8ebd656fe37c82f5ce07b946fce69a
jazzsta <44723405+jazzsta@users.noreply.github.com>
2023-05-10 14:49:59 +0200

    Rscripts

--- src/alinea/topvine/topvine_2022.py
+++ src/alinea/topvine/topvine_2022.py
@@ -7,8 +7,8 @@ import alinea.topvine.data_samples as ds
 
 
 
-def topvine(stand_path='/data/carto.csv', dl_shoot_path='/data/2W_VSP_GRE_ramd.csv',
-            dl_path='/data/Law-leaf-2W-Grenache.csv', shoot_path='/data/ex_rammoy3.csv',
+def topvine(stand_path='/data/carto.csv', shoot_path='/data/ex_rammoy3.csv',
+            dl_shoot_path='/data/2W_VSP_GRE_ramd.csv', dl_path='/data/Law-leaf-2W-Grenache.csv',
             allom_path='/data/allo_Grenache.csv', branches=True, trunk=True, name='geom.csv', geomfile=0):
     # a function that generates all types of scenes based on diverse input
 
