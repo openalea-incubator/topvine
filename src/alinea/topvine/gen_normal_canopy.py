d35f889094871890f95f17bf432d3267e96e778b
jazzsta <44723405+jazzsta@users.noreply.github.com>
2023-05-26 14:43:35 +0200

    workin

--- src/alinea/topvine/gen_normal_canopy.py
+++ src/alinea/topvine/gen_normal_canopy.py
@@ -15,7 +15,9 @@ class gen_normal_canopy(object):
 
     def __call__(self, tab_geom, topol, dl_leaf):
         res = []
+
         for i in range(len(tab_geom)):
+            ### topl = generate_rameau(NFI,NFII,LEN, SF, sd, sd)
             p = []
             for j in range(len(tab_geom[i])):
                 p.append(shoot.Shoot(tab_geom[i][j], topol, dl_leaf))
