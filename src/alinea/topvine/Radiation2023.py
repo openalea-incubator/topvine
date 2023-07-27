b430dab7dd9b80a8676fcdc2a38310e7559401ef
jazzsta <44723405+jazzsta@users.noreply.github.com>
2023-07-27 11:34:53 +0200

    radiation2023

--- src/alinea/topvine/Radiation2023.py
+++ src/alinea/topvine/Radiation2023.py
@@ -31,12 +31,13 @@ params = inputs.params
 
 inputs_hourly = inputs_hourly(psi_soil=inputs.psi_soil, sun2scene=inputs.sun2scene)
 
+meteo_horaire = inputs.weather.loc[inputs.weather.index == inputs.weather.index[12]]
 caribu_source, diffuse_to_total_irradiance_ratio = irradiance_distribution(
-    meteo=inputs_hourly.weather,
+    meteo=meteo_horaire,
     geo_location=params.simulation.geo_location,
     irradiance_unit=params.irradiance.E_type,
     time_zone=params.simulation.tzone,
     turtle_sectors=params.irradiance.turtle_sectors,
     turtle_format=params.irradiance.turtle_format,
-    sun2scene=inputs_hourly.sun2scene,
+    sun2scene=None,
     rotation_angle=params.planting.scene_rotation)
