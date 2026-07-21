272890a588c203588932888f433a5328ed6e9bb6
Rami ALBASHA <rami.albasha@inrae.fr>
2026-07-21 12:32:17 +0200

    refacto(tutorial.py): pep

--- example/tutorial.py
+++ example/tutorial.py
@@ -1,21 +1,16 @@
-""" A python tutorial for runing topvine generator
+""" A python tutorial for running topvine generator
 """
 
+import matplotlib
 
-
+import openalea.topvine.data_samples as ds
 from openalea.topvine.gen_normal_canopy import gen_normal_canopy
 from openalea.topvine.gen_shoot_param import gen_shoot_param
 from openalea.topvine.translate_shoots import translate_shoots
 from openalea.topvine.vine_topiary import vine_topiary
-from openalea.topvine.vine_topiary import topiary
-from openalea.plantgl.all import *
-from openalea.topvine.write_geom_file import write_geom_file
-import openalea.topvine.data_samples as ds
-
-
-import matplotlib
 
 matplotlib.use('Qt5Agg')
+
 
 # python version of topvine/macro/wralea/stand generator composite node
 def _stand_generator(carto, spurs0, dspurs, f_azi, shoot):
@@ -31,26 +26,26 @@ def _stand_generator(carto, spurs0, dspurs, f_azi, shoot):
 
 def main():
     # Plot file  (stand file)    --->   carto [posxyz_plant, nb_coursons]
-        #For every plant, XYZ coordinates + number of shoots
-    stand_path='/data/carto.csv'
+    # For every plant, XYZ coordinates + number of shoots
+    stand_path = '/data/carto.csv'
     # Mean shoot file (shoot file)  --->  ram_moy
-        # 1st line : number of phytomers
-        #  Then     : number of secondary leaves, primary leaf surface area, secondary leaf surface area
-    shoot_path='/data/ex_rammoy3.csv'
+    # 1st line : number of phytomers
+    #  Then     : number of secondary leaves, primary leaf surface area, secondary leaf surface area
+    shoot_path = '/data/ex_rammoy3.csv'
     # Distribution laws for shoot parameters (dl shoot file) ---> 2W_VSP_GRE_ramd
-        # X0,Y0,Z0    : distribution laws for the positioning of the spurs.
-        # DX,DY,DZ    : distribution laws for the distancing of the buds in the spurs.
-        # Dist        : seems not to be used
-        # freq AZI    : frequency of shoot AZI of angle (-20, 20), (20, 160), (160, 200) and (200, 340)
-        # x (     )   : means of the 4 other shoot parameters, namely initial elevation, angle between basal and distal tangents (a.k.a curvature), proportion of shoot accounting for half the curvature and normalized length.
-        # S (    )    : Covariance matrices for the 4 other shoot parameters for each azimuth range.
-    dl_shoot_path='/data/2W_VSP_GRE_ramd.csv'
+    # X0,Y0,Z0    : distribution laws for the positioning of the spurs.
+    # DX,DY,DZ    : distribution laws for the distancing of the buds in the spurs.
+    # Dist        : seems not to be used
+    # freq AZI    : frequency of shoot AZI of angle (-20, 20), (20, 160), (160, 200) and (200, 340)
+    # x (     )   : means of the 4 other shoot parameters, namely initial elevation, angle between basal and distal tangents (a.k.a curvature), proportion of shoot accounting for half the curvature and normalized length.
+    # S (    )    : Covariance matrices for the 4 other shoot parameters for each azimuth range.
+    dl_shoot_path = '/data/2W_VSP_GRE_ramd.csv'
     # Distribution laws for leaf parameters (dl file)  --->  Law-leaf-2W-Grenache
-        # Elevation South – Elevation North – Azimuth South – Azimuth North
-    dl_path='/data/Law-leaf-2W-Grenache.csv'
+    # Elevation South – Elevation North – Azimuth South – Azimuth North
+    dl_path = '/data/Law-leaf-2W-Grenache.csv'
     # Allometry file ---> allo_Grenache
-        # The first line includes the allometric parameters a & b that link the length of a shoot with its number of phytomers (L = a ⋅n+b).
-    allom_path='/data/allo_Grenache.csv'
+    # The first line includes the allometric parameters a & b that link the length of a shoot with its number of phytomers (L = a ⋅n+b).
+    allom_path = '/data/allo_Grenache.csv'
 
     carto = ds.stand_file(stand_path)  #
     spurs0, dspurs, f_azi, shootp = ds.dl_shoot_file(dl_shoot_path)
@@ -64,7 +59,8 @@ def main():
     vt = vine_topiary()
     # boolT toggles trunk visualisation
     # boolI toggles internode visualisation
-    scene = vt(tab_shoot, dl, allometry, boolI=True, boolT=False, boolB=True)   # the last parameter, "False", does nothing
+    scene = vt(tab_shoot, dl, allometry, boolI=True, boolT=False,
+               boolB=True)  # the last parameter, "False", does nothing
 
 
 if __name__ == '__main__':
